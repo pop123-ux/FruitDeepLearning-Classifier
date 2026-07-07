19237b0abbcdde70cce260ab3235e89098a55be9
Pop Alexandru <alexandrupp55@gmail.com>
2026-07-07 18:46:30 +0300

    Update and rename x to main.py

---
+++ EfficientNetV2/main.py
@@ -0,0 +1,70 @@
+import torch
+import os
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torchvision.models import efficientnet_v2_s, EfficientNet_V2_S_Weights
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+transform = transforms.Compose([]
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+dataset_path = r'data'
+fruit_dataset = datasets.ImageFolder(root=dataset_path, transform=transform)
+
+
+batch_size = 32
+dataloader = DataLoader(fruit_dataset, batch_size=batch_size, shuffle=True)
+
+images, labels = next(iter(dataloader))
+
+weights = EfficientNet_V2_S_Weights
+model = efficientnet_v2_s(weights=weights)
+
+num_classes = len(fruit_dataset.classes)
+model.classifier[1] = nn.Linear(in_features=1280, out_features=num_classes)
+
+for param in model.parameters:
+    param.requires_grad = False
+
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
+
+epochs=3
+for epoch in range(epochs):
+    model.train()
+    total_loss = 0.0
+    correct = 0
+    total = 0
+    
+    for images, labels in dataloader:
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item() * images.size(1)
+        _, predicted = torch.max(outputs, 1)
+        batch_correct = (predicted == labels).sum().item()
+        correct_predictions += batch_correct
+        total += labels.size(0)
+
+    epoch_loss = total_loss / total
+    print(f"Epoch {epoch+1}/{epochs} - Loss: {epoch}")
+
+torch.save(model.state_dict(), "best_model.pth")
+print("Model state_dict saved as best_model.pth")
